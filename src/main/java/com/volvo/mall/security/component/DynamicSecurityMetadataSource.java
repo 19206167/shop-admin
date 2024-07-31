2ed62e343b583f4e1d649eac6b4359da54ad2887
Liu Zijian <liuzijian_2021@163.com>
2024-07-31 18:01:16 +0800

    add @Components

--- src/main/java/com/volvo/mall/security/component/DynamicSecurityMetadataSource.java
+++ src/main/java/com/volvo/mall/security/component/DynamicSecurityMetadataSource.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 
@@ -14,6 +15,8 @@ import java.util.*;
 /**
  * 动态权限数据源，用于获取动态权限规则
  */
+
+@Component
 public class DynamicSecurityMetadataSource implements FilterInvocationSecurityMetadataSource {
 
     private static Map<String, ConfigAttribute> configAttributeMap = null;
